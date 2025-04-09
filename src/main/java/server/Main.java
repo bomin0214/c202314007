1638942819f6f8041f3c8182ca6d5c6bd942630b
bomikim0214 <rlaqhals05@naver.com>
2025-04-09 09:43:02 +0900

    three

--- src/main/java/server/Main.java
+++ src/main/java/server/Main.java
@@ -3,5 +3,6 @@ package server;
 public class Main {
     public static void main(String[] args) {
         System.out.println("김보민");
+        World.run();
     }
 }
---
+++ src/main/java/server/World.java
@@ -0,0 +1,8 @@
+package server;
+
+
+public class World {
+    public static void run() {
+        System.out.println("202314007");
+    }
+}
